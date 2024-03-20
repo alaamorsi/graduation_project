b43095f636401be4c88e0f5c477dda6456575733
CADCATSamehAhmed <samehahmedfawzy777next@gmail.com>
2024-03-20 04:08:17 +0200

    add translation arabic and english in few pages

---
+++ lib/language/translation.dart
@@ -0,0 +1,12 @@
+import 'ar.dart';
+import 'en.dart';
+import 'package:get/get.dart';
+
+
+class Translation extends Translations{
+  @override
+  Map<String, Map<String, String>> get keys => {
+    'en':en,
+    'ar':ar,
+  };
+}
