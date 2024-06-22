6033595340825422a54ef0f562c5a465576ed1bb
CADCATSamehAhmed <samehahmedfawzy777next@gmail.com>
2024-06-22 16:23:28 +0300

    fajfsjb

--- lib/modules/student/my_courses/assignments/assignment_webview_screen.dart
+++ lib/modules/student/my_courses/assignments/assignment_webview_screen.dart
@@ -1,19 +1,29 @@
-import 'package:flutter/material.dart';
-import 'package:webview_flutter/webview_flutter.dart';
+import 'dart:async';
 
-class ShowFile extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:flutter_cached_pdfview/flutter_cached_pdfview.dart';
+
+class ShowFile extends StatefulWidget {
   final String url;
   const ShowFile({super.key, required this.url});
+  @override
+  State<ShowFile> createState() => ShowFileState();
+}
 
+class ShowFileState extends State<ShowFile> {
+  final Completer<PDFViewController> _pdfViewController = Completer<PDFViewController>();
+  late PDFViewController shot;
   @override
   Widget build(BuildContext context) {
+    
     return Scaffold(
       appBar: AppBar(),
-      body: WebView(
-        initialUrl:
-        url,
-        javascriptMode: JavascriptMode.unrestricted,
-      ),
+      body: PDF(
+      onViewCreated: (PDFViewController pdfViewController) async {
+        _pdfViewController.complete(pdfViewController);
+        shot = pdfViewController;
+      },
+    ).fromUrl(widget.url),
     );
   }
 }
